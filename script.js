92cac439608a4d9debe478794cc09f656cd2efbd
Daniyal M <itsdaniyalm@gmail.com>
2022-11-18 16:44:07 +0500

    working JS chart example

---
+++ script.js
@@ -0,0 +1,25 @@
+var stars = [135850, 52122, 148825, 16939, 9763];
+var frameworks = ['React', 'Angular', 'Vue', 'Hyperapp', 'Omi'];
+
+var ctx = document.getElementById('myChart');
+
+var stars = [135850, 52122, 148825, 16939, 9763];
+var frameworks = ['React', 'Angular', 'Vue', 'Hyperapp', 'Omi'];
+
+var myChart = new Chart(ctx, {
+type: 'bar',
+data: {
+labels: frameworks,
+datasets: [{
+        label: 'Github Stars',
+        data: stars,
+        backgroundColor: [
+          "#F72585",
+          "#720987",
+          "#3A0CA3",
+          "#4361EE",
+          "#4CC9F0"
+          ]
+    }]
+  }
+})
