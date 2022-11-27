6f53ab56ed01a575972eaef4da509265e5ebf500
Daniyal M <itsdaniyalm@gmail.com>
2022-11-27 20:43:52 +0500

    published pieparrow on pypi, docs update, example page added

--- piesparrow.py
+++ piesparrow.py
@@ -1,3 +1,9 @@
+#######################################################
+# pieSparrow - alpha 0.0.1                            #
+# developed by - Daniyal M (https://itsdaniyalm.com)  #
+# docs - https://piesparrow.itsdaniyalm.com/docs.html #
+#######################################################
+
 import pandas as pd
 import re
 
@@ -7,6 +13,8 @@ beach = ["#99ccff","#99ffff","#009999","#66cccc","#99cccc","#cc9966","#ffcc99","
 forest = ["#333366","#336666","#cc6633","#993300","#99cc66","#999966","#ff6633","#ff9966","#ffcc66"]
 dusktilldawn = ["#9999ff","#6666cc","#666699","#333366","#333333","#663300","#996633","#cc9900","#ffcc00"]
 rainbow = ["#666699","#3399cc","#66cc99","#99cc99","#ffff99","#ffcc99","#ff9966","#ff6633","#cc3366"]
+
+# Initialization function, must call first before and other functions
 
 def init(filename, title, icon=True):
     head = f'''
@@ -43,48 +51,56 @@ def init(filename, title, icon=True):
         file = open(filen,'a')
         file.write(logoHtml)
 
+# H1 text
 def h1(txt):
     h1 = f'''
     <h1>{txt}</h1>
     '''
     return h1
 
+# H2 Text
 def h2(txt):
     h2 = f'''
     <h2>{txt}</h2>
     '''
     return h2
 
+#H3 Text
 def h3(txt):
     h3 = f'''
     <h3>{txt}</h3>
     '''
     return h3
 
+#H4 Text
 def h4(txt):
     h4 = f'''
     <h4>{txt}</h4>
     '''
     return h4
 
+# Paragraph Text
 def p(txt):
     p = f'''
     <p>{txt}</p>
     '''
     return p
 
+# Bold Text
 def bold(txt):
     bold = f'''
     <strong>{txt}</strong>
     '''
     return bold
 
+# Images
 def img(path, height=250, width=250):
     image = f'''
     <img src='{path}' width='{width}' height='{height}'>
     '''
     return image
 
+# Bar Chart
 def bar(title, dataframe, xlabel, ydata, ylabel, titledisplay = 'true', legenddisplay = 'true', color=default, height = '100', width = '100', grid='false', axis='true'):
     df = f'pd.{dataframe}'
     x_label = dataframe[xlabel].tolist()
@@ -136,6 +152,7 @@ def bar(title, dataframe, xlabel, ydata, ylabel, titledisplay = 'true', legenddi
     '''
     return chart
 
+# Line Chart
 def line(title, dataframe, xlabel, ylabel1, ydata1, ylabel2='null', ydata2='null', ylabel3='null', ydata3='null', ylabel4='null', ydata4='null', ylabel5='null', ydata5='null', titledisplay = 'true', legenddisplay = 'true', color=default, height = '100', width = '100', grid='false', axis='true'):
     df = f'pd.{dataframe}'
     x_label = dataframe[xlabel].tolist()
@@ -245,6 +262,7 @@ def line(title, dataframe, xlabel, ylabel1, ydata1, ylabel2='null', ydata2='null
     chart = chart1 + chart2 + chart3 + chart4 + chart5 + chart6
     return chart
 
+# Pie Chart
 def pie(title, dataframe, labels, data, titledisplay = 'true', legenddisplay = 'true', color=default, height = '400', width = '100', grid='false', axis='false'):
     df = f'pd.{dataframe}'
     c_label = dataframe[labels].tolist()
@@ -295,11 +313,13 @@ def pie(title, dataframe, labels, data, titledisplay = 'true', legenddisplay = '
     '''
     return chart
 
+# Dataframe
 def table(df, justify='left'):
     tab = df.to_html(index=False, justify=f'{justify}')
     chart = re.sub("border=\"1\" class=\"dataframe",'',tab)
     return chart
 
+# Grids
 def row(col1='False',col2='False',col3='False',col4='False',col5='False', col1w = 100, col2w = 100, col3w = 100, col4w = 100, col5w = 100):
     start = f'''
     <div class = "container">
